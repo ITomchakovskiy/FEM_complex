36e32b4d90931f634365c0657362c02705575e7f
ITomchakovskiy <pm13.tomchakovskiy@gmail.com>
2025-12-05 02:30:20 +0700

    добавил классы векторов для разных систем координат

---
+++ MKE_complex/Vector/VectorRPhi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKE_complex.Vector;
+
+public class VectorRPhi : VectorBase<double, VectorRPhi>
+{
+    public double R => components![0];
+    public double Phi => components![1];
+    public VectorRPhi(double R, double Phi) => components = [R, Phi];
+    public VectorRPhi(Vector2D V) => components = [V.X, V.Y];
+    protected override VectorRPhi CreateVector(params double[] components) => new VectorRPhi(components[0], components[1]);
+}
---
+++ MKE_complex/Vector/VectorRZ.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKE_complex.Vector;
+
+public class VectorRZ : VectorBase<double, VectorRZ>
+{
+    public double R => components![0];
+    public double Z => components![1];
+    public VectorRZ(double R, double Z) => components = [R, Z];
+    public VectorRZ(Vector2D V) => components = [V.X, V.Y];
+    protected override VectorRZ CreateVector(params double[] components) => new VectorRZ(components[0], components[1]);
+}
