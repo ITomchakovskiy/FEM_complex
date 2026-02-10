630c8a1f5477ae5fc7ad5181e7b44d21bd99f71e
ITomchakovskiy <pm13.tomchakovskiy@gmail.com>
2026-02-10 22:17:33 +0700

    added stiffness matrix for triangle lagrangian 3 order elements for cortesian coordinates

--- MKE_complex/FiniteElements/Elements/LocalMatrices/2D/Lagrangian/TriangleLagrangianLocalMatrices.cs
+++ MKE_complex/FiniteElements/Elements/LocalMatrices/2D/Lagrangian/TriangleLagrangianLocalMatrices.cs
@@ -6,6 +6,27 @@ using System.Threading.Tasks;
 namespace MKE_complex.FiniteElements.Elements.LocalMatrices._2D.Lagrangian;
 public class TriangleLagrangianLocalMatrices
 {
+
+    public static double[][] CalculateLocalStiffnessMatrix(int order, double[,] alphas, double detD, double Coefficient)
+    {
+        switch(order)
+        {
+            case 1: return G1(alphas, detD, Coefficient);
+            case 2: return G2(alphas, detD, Coefficient);
+            case 3: return G3(alphas, detD, Coefficient);
+            default: throw new ArgumentException("wrong element order");
+        }
+    }
+    public static double[][] CalculateLocalMassMatrix(int order, double[,] alphas, double detD, double Coefficient)
+    {
+        switch(order)
+        {
+            case 1: return M1(alphas, detD, Coefficient);
+            case 2: return M2(alphas, detD, Coefficient);
+            case 3: return M3(alphas, detD, Coefficient);
+            default: throw new ArgumentException("wrong element order");
+        }
+    }
     private static double[][] G1(double[,] alphas, double detD, double Coefficient)
     {
         double[][] G = [new double[1],
@@ -33,7 +54,7 @@ public class TriangleLagrangianLocalMatrices
         return M;
     }
 
-    private static double connection1(int[] nums, double[,] alphas)
+    private static double connection21(int[] nums, double[,] alphas)
     {
         double expr1 = 1d;
         double expr2 = 1d;
@@ -44,14 +65,14 @@ public class TriangleLagrangianLocalMatrices
         }
         return expr1 + expr2;
     }
-    private static double connection2(int[] nums, double[,] alphas)
+    private static double connection22(int[] nums, double[,] alphas)
     {
         double expr1 = alphas[nums[0],1] * alphas[nums[0], 1] + alphas[nums[0], 1] * alphas[nums[1], 1] + alphas[nums[1], 1] * alphas[nums[1], 1];
         double expr2 = alphas[nums[0], 2] * alphas[nums[0], 2] + alphas[nums[0], 2] * alphas[nums[1], 2] + alphas[nums[1], 2] * alphas[nums[1], 2];
 
         return expr1 + expr2;
      }
-    private static double connection3(int[] nums, double[,] alphas)
+    private static double connection23(int[] nums, double[,] alphas)
     {
         double expr1 = (alphas[nums[0],1] + alphas[nums[2], 1]) * (alphas[nums[2], 1] + alphas[nums[1], 1]) + alphas[nums[0],1] * alphas[nums[1], 1];
         double expr2 = (alphas[nums[0], 2] + alphas[nums[2], 2]) * (alphas[nums[2], 2] + alphas[nums[1], 2]) + alphas[nums[0], 2] * alphas[nums[1], 2];
@@ -60,30 +81,31 @@ public class TriangleLagrangianLocalMatrices
 
     private static double[][] G2(double[,] alphas, double detD, double Coefficient)
     {
-        double[][] G = new double[6][];
-        G[0] = [1d / 2d * connection1([0,0],alphas)];
-        G[1] = [-1d / 6d * connection1([0,1],alphas),
-                1d / 2d * connection1([1,1],alphas)];
-        G[2] = [-1d / 6d * connection1([0,2],alphas),
-                -1d / 6d * connection1([1,2],alphas),
-                1d / 2d * connection1([2,2],alphas)];
-        G[3] = [2d / 3d * connection1([0,1],alphas),
-                2d / 3d * connection1([0,1],alphas),
-                0d,
-                4d/3d * connection2([0,1],alphas)];
-        G[4] = [0d,
-                2d / 3d * connection1([1,2],alphas),
-                2d / 3d * connection1([1,2],alphas),
-                2d / 3d * connection3([0,2,1],alphas),
-                4d / 3d * connection2([1,2],alphas)];
-        G[5] = [2d / 3d * connection1([0,2],alphas),
-                0d,
-                2d / 3d * connection1([0,2],alphas),
-                2d / 3d * connection3([1,2,0],alphas),
-                2d / 3d * connection3([0,1,2],alphas),
-                4d / 3d * connection2([0,2],alphas)];
-
-        for(int i = 0; i < G.GetLength(0); ++i)
+        double[][] G =
+        [
+            [1d / 2d * connection21([0,0],alphas)],
+            [-1d / 6d * connection21([0,1],alphas),
+                    1d / 2d * connection21([1,1],alphas)],
+            [-1d / 6d * connection21([0,2],alphas),
+                    -1d / 6d * connection21([1,2],alphas),
+                    1d / 2d * connection21([2,2],alphas)],
+            [2d / 3d * connection21([0,1],alphas),
+                    2d / 3d * connection21([0,1],alphas),
+                    0d,
+                    4d/3d * connection22([0,1],alphas)],
+            [0d,
+                    2d / 3d * connection21([1,2],alphas),
+                    2d / 3d * connection21([1,2],alphas),
+                    2d / 3d * connection23([0,2,1],alphas),
+                    4d / 3d * connection22([1,2],alphas)],
+            [2d / 3d * connection21([0,2],alphas),
+                    0d,
+                    2d / 3d * connection21([0,2],alphas),
+                    2d / 3d * connection23([1,2,0],alphas),
+                    2d / 3d * connection23([0,1,2],alphas),
+                    4d / 3d * connection22([0,2],alphas)],
+        ];
+        for (int i = 0; i < G.GetLength(0); ++i)
         {
             for(int j = 0; j <= i; ++j)
                 G[i][j] *= Coefficient * detD;
@@ -128,5 +150,197 @@ public class TriangleLagrangianLocalMatrices
         for(int i = 0; i < M.GetLength(0); ++i)
             M[i] = indices[i].Select(i => el[i] * Coefficient * detD).ToArray();
         return M;
+    }
+
+    private static double connection31(int[] nums, double[,] alphas)
+    {
+        if(nums.Count() != 1) throw new ArgumentException();
+        int num = nums[0];
+
+        double expr1 = alphas[num,1] * alphas[num,1];
+        double expr2 = alphas[num,2] * alphas[num,2];
+
+        return expr1 + expr2;
+    }
+
+    private static double connection32(int[] nums, double[,] alphas)
+    {
+        if(nums.Count() != 2) throw new ArgumentException();
+        
+        double expr1 = alphas[nums[0],1] * alphas[nums[1],1];
+        double expr2 = alphas[nums[0],2] * alphas[nums[1],2];
+
+        return expr1 + expr2;
+    }
+
+    private static double connection33(int[] nums, double[,] alphas)
+    {
+        if(nums.Count() != 2) throw new ArgumentException();
+        
+        double expr1 = connection31([nums[0]], alphas);
+        double expr2 = 19d * connection32(nums, alphas);
+        
+        return expr1 + expr2;
+    }
+
+    private static double connection34(int[] nums, double[,] alphas)
+    {
+        if(nums.Count() != 2) throw new ArgumentException();
+        
+        double expr1 = connection31([nums[0]], alphas);
+        double expr2 = -8d * connection32(nums, alphas);
+        
+        return expr1 + expr2;
+    }
+
+    private static double connection35(int[] nums, double[,] alphas)
+    {
+        if(nums.Count() != 3) throw new ArgumentException();
+        
+        double expr1 = connection32([nums[0], nums[1]], alphas);
+        double expr2 = connection32([nums[0], nums[2]], alphas);
+
+        return expr1 + expr2;
+    }
+
+    private static double connection36(int[] nums, double[,] alphas)
+    {
+        if(nums.Count() != 2) throw new ArgumentException();
+        
+        double expr1 = connection31([nums[0]],alphas);
+        double expr2 = connection31([nums[1]],alphas);
+        double expr3 = connection32(nums,alphas);
+
+        return expr1 + expr2 + expr3;
+    }
+
+    private static double connection37(int[] nums, double[,] alphas)
+    {
+        if(nums.Count() != 2) throw new ArgumentException();
+        
+        double expr1 = alphas[nums[0], 1] - alphas[nums[1], 1];
+        double expr2 = expr1 * expr1;
+        double expr3 = alphas[nums[0], 2] - alphas[nums[1], 2];
+        double expr4 = expr3 * expr3;
+
+        return expr2 + expr4;
+    }
+
+    private static double connection38(int[] nums, double[,] alphas)
+    {
+        if(nums.Count() != 3) throw new ArgumentException();
+        
+        double expr1 = connection31([nums[0]],alphas);
+        double expr2 = 2d * connection32([nums[1], nums[2]],alphas);
+        double expr3 = connection32([nums[0], nums[1]],alphas);
+        double expr4 = connection32([nums[0], nums[2]],alphas);
+
+        return expr1 + expr2 + expr3 + expr4;
+    }
+
+    private static double connection39(int[] nums, double[,] alphas)
+    {
+        if(nums.Count() != 1) throw new ArgumentException();
+        
+        double expr1 = alphas[nums[0],1] * (alphas[0,1] + alphas[1,1] + alphas[2,1]);
+        double expr2 = alphas[nums[0],2] * (alphas[0,2] + alphas[1,2] + alphas[2,2]);
+        return expr1 + expr2;
+    }
+
+    private static double connection310(double[,] alphas)
+    {
+        double expr1 = connection31([0],alphas) +
+                       connection31([1],alphas) +
+                       connection31([2],alphas);
+        double expr2 = connection32([0, 1], alphas) +
+                       connection32([1, 2], alphas) +
+                       connection32([0, 2], alphas);
+        return expr1 + expr2;
+    }
+
+    private static double[][] G3(double[,] alphas, double detD, double Coefficient)
+    {
+        double[][] G =
+        [
+            [17d/40d * connection31([0], alphas)],
+            [
+                7d/80d * connection32([0,1],alphas),
+                17d/40d * connection31([1], alphas)
+            ],
+            [
+                7d/80d * connection32([0,2],alphas),
+                7d/80d * connection32([1,2],alphas),
+                17d/40d * connection31([2], alphas)
+            ],
+            [
+                3d/80d * connection33([0,1],alphas),
+                3d/80d * connection34([1,0],alphas),
+                3d/80d * connection35([2,0,1],alphas),
+                27d/16d * connection36([0,1],alphas)
+            ],
+            [
+                3d/80d * connection34([0,1],alphas),
+                3d/80d * connection33([1,0],alphas),
+                3d/80d * connection35([2,0,1],alphas),
+                -27d/80d * connection37([0,1],alphas),
+                27d/16d * connection36([0,1],alphas)
+            ],
+            [
+                3d/80d * connection35([0,1,2],alphas),
+                3d/80d * connection33([1,2],alphas),
+                3d/80d * connection34([2,1],alphas),
+                -27d/160d * connection38([1, 0, 2],alphas),
+                27d/32d * connection38([1, 0, 2],alphas),
+                27d/16d * connection36([1,2],alphas)
+            ],
+            [
+                3d/80d * connection35([0,1,2],alphas),
+                3d/80d * connection34([1,2],alphas),
+                3d/80d * connection33([2,1],alphas),
+                -27d/160d * connection38([1, 0, 2],alphas),
+                -27d/160d * connection38([1, 0, 2],alphas),
+                -27d/80d * connection37([1,2],alphas),
+                27d/16d * connection36([1,2],alphas)
+            ],
+            [
+                3d/80d * connection34([0,2],alphas),
+                3d/80d * connection35([1,0,2],alphas),
+                3d/80d * connection33([2,0],alphas),
+                -27d/160d * connection38([0, 1, 2],alphas),
+                -27d/160d * connection38([0, 1, 2],alphas),
+                -27d/160d * connection38([2, 1, 0],alphas),
+                27d/32d * connection38([2, 0, 1],alphas),
+                27d/16d * connection36([0,2],alphas)
+            ],
+            [
+                3d/80d * connection33([0,2],alphas),
+                3d/80d * connection35([1,0,2],alphas),
+                3d/80d * connection34([2,0],alphas),
+                27d/32d * connection38([0, 1, 2],alphas),
+                -27d/160d * connection38([0, 1, 2],alphas),
+                -27d/160d * connection38([2, 0, 1],alphas),
+                -27d/160d * connection38([2, 0, 1],alphas),
+                -27d/80d * connection37([0,2],alphas),
+                27d/16d * connection36([0,2],alphas)
+            ],
+            [
+                9d/80d * connection39([0],alphas),
+                9d/80d * connection39([1],alphas),
+                9d/80d * connection39([2],alphas),
+                81d/80d * connection38([1, 0, 2],alphas),
+                81d/80d * connection38([0, 1, 2],alphas),
+                81d/80d * connection38([2, 1, 0],alphas),
+                81d/80d * connection38([1, 2, 0],alphas),
+                81d/80d * connection38([0, 2, 1],alphas),
+                81d/80d * connection38([2, 0, 1],alphas),
+                81d/20d * connection310(alphas)
+            ]
+        ];
+        for (int i = 0; i < G.GetLength(0); ++i)
+        {
+            for(int j = 0; j <= i; ++j)
+                G[i][j] *= Coefficient * detD;
+        }
+        return G;
     }
 }
